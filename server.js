080ac1c550d901bd83c3e2eb3a0d750eec2f2a05
Dominique <dfoleydev@gmail.com>
2022-06-02 20:59:46 -0400

    Working rapper api

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        age: 29,
+        birthName: 'Sheyaa Bin Abraham-Joseph',
+        birthLocation: 'London, England',
+    },
+    'chance the rapper': {
+        age: 29,
+        birthName: 'Chancelor Bennett',
+        birthLocation: 'Chicago, Illinois',
+    },
+    unknown: {
+        age: 0,
+        birthname: 'unknown',
+        birthLocation: 'unknown',
+    },
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        res.json(rappers[rapperName])
+    } else {
+        res.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+})
