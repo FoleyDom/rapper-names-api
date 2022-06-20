f295ebe322d0562281fbbab3cf8c60f5b93a8428
Dominique <dfoleydev@gmail.com>
2022-06-19 20:29:04 -0400

    Changes

---
+++ main.js
@@ -0,0 +1,17 @@
+const rappers = {
+    '21 savage': {
+        age: 29,
+        birthName: 'Sheyaa Bin Abraham-Joseph',
+        birthLocation: 'London, England',
+    },
+    'chance the rapper': {
+        age: 29,
+        birthName: 'Chancelor Bennett',
+        birthLocation: 'Chicago, Illinois',
+    },
+    unknown: {
+        age: 0,
+        birthname: 'unknown',
+        birthLocation: 'unknown',
+    },
+}
--- server.js
+++ server.js
@@ -2,50 +2,30 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-const rappers = {
-	'21 savage': {
-		age: 29,
-		birthName: 'Sheyaa Bin Abraham-Joseph',
-		birthLocation: 'London, England',
-	},
-	'chance the rapper': {
-		age: 29,
-		birthName: 'Chancelor Bennett',
-		birthLocation: 'Chicago, Illinois',
-	},
-	unknown: {
-		age: 0,
-		birthname: 'unknown',
-		birthLocation: 'unknown',
-	},
-}
-
 app.get('/', (request, response) => {
-	response.sendFile(__dirname + '/index.html')
+    response.sendFile(__dirname + '/index.html')
 })
 
 app.get('/api/:name', (req, res) => {
-	const rapperName = req.params.name.toLowerCase()
-	if (rappers[rapperName]) {
-		res.json(rappers[rapperName])
-		console.log(rappers[rapperName])
-	} else {
-		res.json(rappers['unknown'])
-		console.log(rappers[rapperName])
-	}
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        res.json(rappers[rapperName])
+        console.log(rappers[rapperName])
+    } else {
+        res.json(rappers['unknown'])
+        console.log(rappers[rapperName])
+    }
 })
 
-app.post('/api/:name', (req, res) => {
-	rappers
-		.push()
-		// .then((result) => {
-		// 	res.redirect('/')
-		// })
-		.catch((error) => console.error(error))
+app.post('/main.js', (req, res) => {
+    rappers
+        .push()
+        // .then((result) => {
+        // 	res.redirect('/')
+        // })
+        .catch((error) => console.error(error))
 })
 
 app.listen(PORT, () => {
-	console.log(
-		`The server is now running on port ${PORT}! Betta Go Catch It!!`
-	)
+    console.log(`The server is now running on port ${PORT}!`)
 })
